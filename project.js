6d0ad3b1c4882677bf97ffcc7f610329d3a51d40
TheHectorSector <40252285+TheHectorSector@users.noreply.github.com>
2019-01-10 05:05:21 -0600

    big oof

--- project.js
+++ project.js
@@ -1,7 +1,48 @@
-var btn = document.querySelector(".sai");
-var getText = document.querySelector(".getText");
-var content = document.querySelector(".getcontent");
-var editorContent = document.querySelector(".editor");
+
+
+window.addEventListener("load", function(){
+  //first check if execCommand and contentEditable attribute is supported or not.
+  if(document.contentEditable != undefined && document.execCommand != undefined)
+ {
+     alert("HTML5 Document Editing API Is Not Supported");
+  }
+  else
+  {
+      document.execCommand('styleWithCSS', false, true);
+  }
+}, false);
+
+//underlines the selected text
+function underline()
+{
+  document.execCommand("underline", false, null);
+}
+
+function italic() {
+  document.execCommand("italic", false, null);
+}
+
+function bold() {
+  document.execCommand("bold", false, null);
+}
+
+function strike() {
+  document.execCommand("strikeThrough", false, null);
+}
+
+//makes the selected text as hyperlink.
+function link()
+{
+  var url = prompt("Enter the URL");
+  document.execCommand("createLink", false, url);
+}
+
+//displays HTML of the output
+function displayhtml()
+{
+  //set textContent of pre tag to the innerHTML of editable div. textContent can take any form of text and display it as it is without browser interpreting it. It also preserves white space and new line characters.
+  document.getElementsByClassName("htmloutput")[0].textContent = document.getElementsByClassName("editor")[0].innerHTML;
+}
 
 btn.addEventListener("click", function() {
   var s = editorContent.innerHTML;
@@ -28,5 +69,4 @@ function changeColor() {
   var color = prompt("Enter your color in hex: ");
   document.execCommand("foreColor", false, color);
 }
-
 
